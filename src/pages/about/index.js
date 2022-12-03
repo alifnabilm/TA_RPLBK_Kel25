10c4f4ffaf1b01a4cdc2d4f4f9405a55f88df77f
yusuf hammadi <ucup1745@gmail.com>
2022-12-03 22:51:52 +0700

    Create index.js

---
+++ src/pages/about/index.js
@@ -0,0 +1,60 @@
+export default function About(){
+    return(
+        <section style={{textAlign: "center", display: "flex", height: "100vh"}}>
+            <div style={{margin: 'auto'}}>
+                <h2 style={{color: '#FF9F45'}}>
+                    About App
+                </h2>
+                <p>
+                    Aplikasi ini merupakan aplikasi membaca buku beta yang menggunakan dua API yang pertama laravel db books dan open library API. Aplikasi ini dibuat oleh kelompok 25 beranggotakan
+                </p>
+                <table style={{marginTop: "10px"}}>
+                    <tr>
+                        <td>
+                            MUHAMMAD DZIKRULLAH FARHAN
+                        </td>
+                        <td>
+                            :
+                        </td>
+                        <td>
+                            21120119140134
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Alif Nabil Musyaffa
+                        </td>
+                        <td>
+                            :
+                        </td>
+                        <td>
+                            21120119130078
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            YUSUF HAMMADI
+                        </td>
+                        <td>
+                            :
+                        </td>
+                        <td>
+                            21120118140092
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Shafiyah Huyai
+                        </td>
+                        <td>
+                            :
+                        </td>
+                        <td>
+                            21120119120004
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </section>
+    )
+}
